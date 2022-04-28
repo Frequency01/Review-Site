191769bec25dd31c36ba722a21678b9a564e118f
Frequency01 <meowharry@mail.ru>
2022-04-28 16:43:11 +0300

    create components for layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
-import { Button, Htag, Ptag, Rating, Tag} from "../components";
+import { Button, Htag, Ptag, Rating, Tag } from "../components";
+import { withLayout } from "../layout/Layout/Layout";
 
 
-export default function Home(): JSX.Element {
-  const[rating, setRating] = useState<number>(4)
+function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4)
   return (
-    <div>
+    <>
       <Htag tag="h1">fjefjj</Htag>
       <Button appearance="primary" arrow="right">Кнопка</Button>
       <Button appearance="ghost" arrow="down">Кнопка</Button>
       <Ptag size="large">Я текст</Ptag>
       <Tag size='small' color="red">Hopa</Tag>
       <Tag size='medium' color="green">Popa</Tag>
-      <Rating rating={rating} isEditable setRating={setRating}/>
-    </div>
+      <Rating rating={rating} isEditable setRating={setRating} />
+    </>
   );
 }
+export default withLayout(Home)
