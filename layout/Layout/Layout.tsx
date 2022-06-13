ec28d4a10f4f9df71a15c1dfaae4dd90ba4b0994
Frequency01 <meowharry@mail.ru>
2022-06-13 23:20:45 +0300

    add adaptive footer, add ate-fns

--- layout/Layout/Layout.tsx
+++ layout/Layout/Layout.tsx
@@ -3,20 +3,18 @@ import {Header} from '../Header/Header'
 import {Sidebar} from '../Sidebar/Sidebar'
 import {Footer} from '../Footer/Footer'
 import styles from "./Layout.module.css"
-import { FunctionComponent, FunctionComponentFactory } from "react"
+import { FunctionComponent } from "react"
 
  const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
-        <>
-            <Header />
-            <div>
-                <Sidebar />
-                <div>
+        <div className={styles.wrapper}>
+            <Header className={styles.header}/>
+                <Sidebar className={styles.sidebar}/>
+                <div className={styles.body}>
                     {children}
                 </div>
-            </div>
-            <Footer />
-        </>
+            <Footer className={styles.footer}/>
+        </div>
     )
 }
 
